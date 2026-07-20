10879d664b620ffd49bce90e99f755b6713f8454
Ubuntu <ubuntu@ip-172-31-23-96.ec2.internal>
2026-07-20 15:56:34 +0000

    Adding a heading to verify pipeline status

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const http = require('http');
 // Create a server object
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'}); 
-  res.write('<h1>🚀 Mentor Checkpoint: My CI/CD Pipeline is Working!</h1>');
+  res.write('<h1> Mentor Checkpoint: My CI/CD Pipeline is Working!</h1>');
   res.write('<h1> Making changes on this file to see if it immediately pushes live with CICD magic!</h1>');
+  res.write('<h2> Whats up CICD Pipeline - Update 20th July, 2026</h2>');
   res.write('<p>Status: Online</p>');
   res.end(); 
 }).listen(80); // The server listens on port 80 (standard HTTP)
