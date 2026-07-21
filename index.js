1b715965162c77fec6a047c178293d0833946019
Yoginder Bagga <yoginder.bagga@gmail.com>
2026-07-21 09:28:46 +0000

    Fixed a syntax

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'}); 
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'}); 
 
   res.end(`
     <!DOCTYPE html>
